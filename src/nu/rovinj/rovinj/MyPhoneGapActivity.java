4638f86dabf7a9684afe7c7b3dee8a2ddd1b76fa
Edvard Boras <edvard123@hotmail.com>
2013-09-20 16:38:04 +0200

    rovinj

---
+++ src/nu/rovinj/rovinj/MyPhoneGapActivity.java
@@ -0,0 +1,13 @@
+package nu.rovinj.rovinj;
+
+import org.apache.cordova.DroidGap;
+import android.os.Bundle;
+
+public class MyPhoneGapActivity extends DroidGap {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.setIntegerProperty("splashscreen", R.drawable.splash);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
